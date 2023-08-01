9e2e4d6dc03423215b78cf7b0d3f77af2784b25b
fmautinoh <118001450+fmautinoh@users.noreply.github.com>
2023-08-01 01:56:56 -0500

    Libro Create OK

--- api-bibliotecaICL/Controllers/LibroController.cs
+++ api-bibliotecaICL/Controllers/LibroController.cs
@@ -29,28 +29,28 @@ namespace Api_Inventariobiblioteca.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
-        [Route("/ListaLibro")]
-        [ProducesResponseType(200)]//ok
-        [ProducesResponseType(400)]//badreq
-        [ProducesResponseType(404)]//no found
-        public async Task<ActionResult<APIResponse>> GetLibro()
-        {
-            try
-            {
-                IEnumerable<LibroDto> autorlist = (IEnumerable<LibroDto>) await _vistalibrorepo.ListObjetos();
-                _apiResponse.Alertmsg = "Listado Exitosamente";
-                _apiResponse.Resultado = _mapper.Map<IEnumerable<LibroDto>>(autorlist);
-                _apiResponse.StatusCode = HttpStatusCode.OK;
-                return Ok(_apiResponse);
-            }
-            catch (Exception ex)
-            {
-                _apiResponse.IsSuccess = false;
-                _apiResponse.ErrorMessage = new List<string> { ex.ToString() };
-            }
-            return _apiResponse;
-        }
+        //[HttpGet]
+        //[Route("/ListaLibro")]
+        //[ProducesResponseType(200)]//ok
+        //[ProducesResponseType(400)]//badreq
+        //[ProducesResponseType(404)]//no found
+        //public async Task<ActionResult<APIResponse>> GetLibro()
+        //{
+        //    try
+        //    {
+        //        IEnumerable<LibroDto> autorlist = (IEnumerable<LibroDto>) await _vistalibrorepo.ListObjetos();
+        //        _apiResponse.Alertmsg = "Listado Exitosamente";
+        //        _apiResponse.Resultado = _mapper.Map<IEnumerable<LibroDto>>(autorlist);
+        //        _apiResponse.StatusCode = HttpStatusCode.OK;
+        //        return Ok(_apiResponse);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _apiResponse.IsSuccess = false;
+        //        _apiResponse.ErrorMessage = new List<string> { ex.ToString() };
+        //    }
+        //    return _apiResponse;
+        //}
 
 
         [HttpPost]
